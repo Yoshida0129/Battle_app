ca1254dab51cc009ba9ebd648eca539b0bff006f
Tatsuya Yoshida <alpacagame0129@gmail.com>
2019-05-20 06:16:57 +0900

    Add:style

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,29 @@
 <template>
-  <router-view id="app" />
+  <router-view />
 </template>
 
 <style>
-#app {
+body {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   margin: 0;
+  height: 100vh;
+  background-color: #77aa77;
+  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100%25' height='100%25' viewBox='0 0 2 1'%3E%3Cdefs%3E%3ClinearGradient id='a' gradientUnits='userSpaceOnUse' x1='0' x2='0' y1='0' y2='1'%3E%3Cstop offset='0' stop-color='%2377aa77'/%3E%3Cstop offset='1' stop-color='%234fd'/%3E%3C/linearGradient%3E%3ClinearGradient id='b' gradientUnits='userSpaceOnUse' x1='0' y1='0' x2='0' y2='1'%3E%3Cstop offset='0' stop-color='%23cf8' stop-opacity='0'/%3E%3Cstop offset='1' stop-color='%23cf8' stop-opacity='1'/%3E%3C/linearGradient%3E%3ClinearGradient id='c' gradientUnits='userSpaceOnUse' x1='0' y1='0' x2='2' y2='2'%3E%3Cstop offset='0' stop-color='%23cf8' stop-opacity='0'/%3E%3Cstop offset='1' stop-color='%23cf8' stop-opacity='1'/%3E%3C/linearGradient%3E%3C/defs%3E%3Crect x='0' y='0' fill='url(%23a)' width='2' height='1'/%3E%3Cg fill-opacity='0.5'%3E%3Cpolygon fill='url(%23b)' points='0 1 0 0 2 0'/%3E%3Cpolygon fill='url(%23c)' points='2 1 2 0 0 0'/%3E%3C/g%3E%3C/svg%3E");
+  background-attachment: fixed;
+  background-size: cover;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+button {
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  outline: none;
+  appearance: none;
+  padding: 10px;
+  box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.3);
 }
 </style>
--- src/views/Battle.vue
+++ src/views/Battle.vue
@@ -1,34 +1,33 @@
 <template>
-  <div>
-    <button @click="_choice(1)" :disabled="is_loading">
-      ぱー
-    </button>
-    <button @click="_choice(0)" :disabled="is_loading">
-      ちょき
-    </button>
-    <button @click="_choice(2)" :disabled="is_loading">
-      ぐー
-    </button>
-    {{ my_hp }}
-    {{ enemy_hp }}
-    <div
-      @click="
-        is_modal_open = false;
-        is_loading = false;
-        msg = '';
-      "
-      v-if="is_modal_open"
-    >
-      <modal :msg="msg" />
+  <div id="battle-form">
+    <p>敗北者になった回数{{ lose_count }}</p>
+    <section v-if="!is_modal_open && !is_finished">
+      <p>自分のHP:{{ my_hp }}</p>
+      <p>敵のHP:{{ enemy_hp }}</p>
+    </section>
+    <div class="button-container" v-if="!is_modal_open && !is_finished">
+      <button @click="_choice(1)" :disabled="is_loading">
+        <i class="fas fa-hand-paper paper-icon"></i>
+      </button>
+      <button @click="_choice(0)" :disabled="is_loading">
+        <i class="fas fa-hand-scissors scissors-icon"></i>
+      </button>
+      <button @click="_choice(2)" :disabled="is_loading">
+        <i class="fas fa-hand-rock rock-icon"></i>
+      </button>
     </div>
-    <div v-if="is_finished">
+    <div class="modal-background" v-else></div>
+    <div v-if="is_modal_open" class="modal-container">
+      <modal :msg="msg" @next="_next"></modal>
+    </div>
+    <div v-else-if="is_finished" class="modal-container">
       <modal
         :msg="msg"
         :is_win="is_win"
         :is_finished="true"
         :lose_count="lose_count"
-        @retry="retry"
-      />
+        @retry="_retry"
+      ></modal>
     </div>
   </div>
 </template>
@@ -46,11 +45,11 @@ export default Vue.extend({
   name: "Battle",
   data() {
     return {
-      my_hp: 1,
-      enemy_hp: 1,
+      my_hp: 3,
+      enemy_hp: 3,
       lose_count: 0,
       is_loading: false,
-      sec: 3000,
+      sec: 200,
       is_modal_open: false,
       msg: "",
       is_finished: false,
@@ -66,13 +65,20 @@ export default Vue.extend({
         this.is_finished = true;
         this.lose_count++;
         this.is_win = false;
+        this.msg = "YOU LOSE";
+      } else {
+        this.is_modal_open = true;
+        this.msg = "自分に1ダメージ";
       }
     },
     enemy_hp: function() {
       if (this.enemy_hp <= 0) {
         this.is_finished = true;
-        this.msg = "Win";
         this.is_win = true;
+        this.msg = "YOU WIN";
+      } else {
+        this.is_modal_open = true;
+        this.msg = "敵に1ダメージ！";
       }
     }
   },
@@ -83,25 +89,88 @@ export default Vue.extend({
     },
     _judge(user_choice: number): void {
       _judgePoint(user_choice).then((val: number) => {
-        this.is_modal_open = true;
         if (val === 2) {
           this.my_hp--;
-          this.msg = "Lose";
         } else if (val === 1) {
           this.enemy_hp--;
-          this.msg = "Win";
         } else {
           this.msg = "Draw";
+          this.is_modal_open = true;
         }
       });
     },
-    retry(): void {
-      this.my_hp = 1;
-      this.enemy_hp = 1;
-      this.is_finished = false;
+    _retry(init?: boolean): void {
+      this.my_hp = 3;
+      this.enemy_hp = 3;
       this.msg = "";
+      this.is_modal_open = false;
+      this.is_finished = false;
+      this.is_modal_open = false;
       this.is_loading = false;
+      if (init) {
+        this.lose_count = 0;
+      }
+    },
+    _next(): void {
+      this.is_modal_open = false;
+      this.is_finished = false;
+      this.is_loading = false;
+      this.msg = "";
     }
   }
 });
 </script>
+<style lang="scss" scoped>
+#battle-form {
+  width: 50%;
+  height: 50%;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(255, 255, 255, 0.3);
+  border-radius: 10px;
+  
+}
+.button-container {
+  width: 80%;
+  height: 30%;
+  margin: 0 auto;
+  > button {
+    width: calc(100% / 3);
+    height: calc(100% / 3);
+    > i {
+      font-size: 20px;
+    }
+  }
+  .paper-icon {
+    color: #aa0000;
+  }
+  .scissors-icon {
+    color: #00aa00;
+    transform: rotate(90deg) scale(1, -1);
+  }
+  .rock-icon {
+    color: #0000aa;
+  }
+}
+.modal-background {
+  z-index: 2;
+  height: 100vh;
+  width: 100vw;
+  background-color: rgba(0, 0, 0, 0.3);
+  position: absolute;
+}
+.modal-container {
+  width: 50%;
+  height: 50%;
+  background-color: rgba(255, 255, 255, 0.3);
+  border-radius: 10px;
+  z-index: 3;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+}
+</style>
